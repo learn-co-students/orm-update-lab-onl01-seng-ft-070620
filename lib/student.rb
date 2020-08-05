1b6dc40cf045515609f6d6480b27dd8a58040fbb
Ethan <eleetyson@gmail.com>
2020-08-05 18:55:32 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,25 +1,12 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :id, :name, :grade
 
   def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
   end
-
-  # def self.create_table
-  #   sql = <<-SQL
-  #     CREATE TABLE IF NOT EXISTS students (
-  #       id INTEGER PRIMARY KEY,
-  #       name TEXT,
-  #       grade TEXT
-  #     )
-  #   SQL
-  #
-  #   DB[:conn].execute(sql)
-  # end
 
 end
