94ff0a626ae1e066ff3acad8ce7a3e0be80b0b60
Ethan <eleetyson@gmail.com>
2020-08-05 20:30:54 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,7 @@ class Student
     @grade = grade
   end
 
+# create a database table with columns matching the attributes for a Student instance
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
@@ -21,11 +22,16 @@ class Student
     DB[:conn].execute(sql)
   end
 
+# delete the students database table
   def self.drop_table
     sql = "DROP TABLE students"
     DB[:conn].execute(sql)
   end
 
+# if the student instance already has a corresponding table row, update that row
+# if not...
+# insert a new row into the students table with the attributes of the student instance and...
+# assign that row's id value as an attribute to the student instance
   def save
     if self.id
       self.update
@@ -36,22 +42,26 @@ class Student
     end
   end
 
+# update the corresponding table row for the student instance
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+# create a student instance, save it to the students table, and return the instance
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
+# creates and returns a new Student object using a row from the students table passed as an argument
   def self.new_from_db(row)
     student = self.new(row[0], row[1], row[2])
     student
   end
 
+# returns a student object for the table row corresponding to the name passed as an argument
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE students.name = ? LIMIT 1"
 
